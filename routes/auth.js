0f541aa985f920961905c55b34649ed8a9e547b1
xinjuan deng <dengxinj@gmail.com>
2021-05-03 10:31:46 -0400

    adding validator schema

--- routes/auth.js
+++ routes/auth.js
@@ -4,15 +4,30 @@ const express = require("express");
 const router = new express.Router();
 const User = require("../model/users");
 const { createToken } = require("../helpers/tokens");
-const { ExpressError } = require("../expressError");
+const { BadRequestError } = require("../expressError");
+const jsonschema = require("jsonschema");
+const userRegisterSchema = require("../json_schema.js/userRegisterSchema.json");
+const userAuth = require("../json_schema.js/userAuth.json");
 
 /**register route, authoration: None. It will return token
  * * POST /auth/register:   { user } => { token }
  * user must include { username, password, firstName, lastName, email }
+ * {
+	"username": "dog",
+	"password":"123456",
+	"firstName":"dog",
+	"lastName":"mark",
+	"email":"dog@gmail.com"
+}
  */
 
 router.post("/register", async function (req, res, next) {
   try {
+    const validator = jsonschema.validate(req.body, userRegisterSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
     const newUser = await User.register({ ...req.body });
     const token = createToken(newUser);
     return res.status(201).json({ token });
@@ -21,17 +36,9 @@ router.post("/register", async function (req, res, next) {
   }
 });
 
-/**{
-	"username": "dog",
-	"password":"123456",
-	"firstName":"dog",
-	"lastName":"mark",
-	"email":"dog@gmail.com"
-	
-}
-*/
-
 /** POST /auth/token:  { username, password } => { token }
+ * {"username":"dog",
+ * "password":"123456"}
  *
  * Returns JWT token which can be used to authenticate further requests.
  *
@@ -40,11 +47,12 @@ router.post("/register", async function (req, res, next) {
 
 router.post("/token", async function (req, res, next) {
   try {
-    /*const validator = jsonschema.validate(req.body, userAuthSchema);
+    const validator = jsonschema.validate(req.body, userAuth);
     if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
+      const errs = validator.errors.map((e) => e.stack);
+      console.log(errs);
       throw new BadRequestError(errs);
-    }*/
+    }
 
     const { username, password } = req.body;
     const user = await User.authenticate(username, password);
